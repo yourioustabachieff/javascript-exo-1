d05b3e155bd09d24016ea0086351a096086c5445
yourioustabachieff <yourioustabachieff@gmail.com>
2021-07-19 13:49:44 +0200

    javascritp partie 2 exo 1

--- exo1.js
+++ exo1.js
@@ -1,7 +1,11 @@
-let person = prompt("Please enter your name", "Harry Potter");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
-  text = "Hello " + person + "! How are you today?";
-} 
+var number = 3;
+
+function resultat (){
+
+resultat = number * 2-3
+
+}
+
+resultat()
+
+console.log(resultat)

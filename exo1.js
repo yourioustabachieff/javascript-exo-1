8d0bacc7db4d6e3c4f0153dd1b645cffa685ca2e
yourioustabachieff <yourioustabachieff@gmail.com>
2021-07-19 14:10:25 +0200

    javascript partie 2 exo 3

--- exo1.js
+++ exo1.js
@@ -1,13 +1,14 @@
 var number = 3;
 
-function resultat (){
 
-resultat = number / 2-3
+function resultat (nb1){
+
+resultat = nb1 * 2-3
 alert(resultat)
 
 }
 
-resultat()
+resultat(number)
 
 console.log(resultat)
 

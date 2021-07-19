f364c57e338d1bfbeda16fb3d02c985e8d912df4
yourioustabachieff <yourioustabachieff@gmail.com>
2021-07-19 10:26:40 +0200

    exo 5

--- exo1.js
+++ exo1.js
@@ -1,7 +1,7 @@
-
-var lastname = "lastname\n";
-var firstname = "firstname\n";
-var city = "city\n";
-
-
-alert(lastname + firstname + city)
+let person = prompt("Please enter your name", "Harry Potter");
+let text;
+if (person == null || person == "") {
+  text = "User cancelled the prompt.";
+} else {
+  text = "Hello " + person + "! How are you today?";
+} 
